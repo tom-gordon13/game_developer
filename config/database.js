cbd7266e0eea19410f5d93d1c6232d3e3518126b
tom-gordon13 <tombcgordon@gmail.com>
2023-07-22 09:48:15 -0600

    fix mongodb connection issues

--- config/database.js
+++ config/database.js
@@ -1,6 +1,30 @@
 const mongoose = require('mongoose')
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
-console.log(process.env.MONGO_DATABASE_URL)
+// const client = new MongoClient(process.env.MONGO_DATABASE_URL, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+
+// run().catch(console.dir);
+
+
 mongoose.connect(process.env.MONGO_DATABASE_URL)
 
 const db = mongoose.connection;
